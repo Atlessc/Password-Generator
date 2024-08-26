2c81f222938e380f47e96bcd02a54835242ee9dc
Tyler Smith <tylersmith4596@gmail.com>
2024-08-25 17:57:59 -0700

    feat: Add StreetNameAnagram algorithm to options
    The StreetNameAnagram algorithm has been added to the options list. This algorithm generates an anagram of a given street name, providing users with a new algorithm option.

--- src/data/options.js
+++ src/data/options.js
@@ -15,54 +15,48 @@ export const optionDetails = [
     shortName: "Daily Products Used",
     label: "Enter three different names of products you use daily, separated by semicolons, and we'll use the first letters of each to create part of your password.",
     algo: "getDailyProductsUsed",
-    type: "text", // This option requires a text input for the products
+    type: "text", 
   },
-  // {
-  //   shortName: "Street Name Anagram",
-  //   label: "Enter a street names, and we'll use its anagram to create part of your password.",
-  //   algo: "getStreetNameAnagram",
-  //   type: "text", // This option requires a text input for the street name
-  // },
+  {
+    shortName: "Street Name Anagram",
+    label: "Enter a street names, and we'll use its anagram to create part of your password.",
+    algo: "getStreetNameAnagram",
+    type: "text",
+  },
   {
     shortName: "Common Word Anagram",
     label: "Enter a word, and we'll create an anagram to use as part of your password.",
     algo: "getCommonWordAnagram",
-    type: "text", // This option requires a text input for the word
+    type: "text", 
   },
   // {
   //   shortName: "Character Name",
   //   label: "Enter the name of a character from a story you closely relate to, and we'll combine it with a random number to create part of your password.",
   //   algo: "getCharacterName",
-  //   type: "text", // This option requires a text input for the character name
+  //   type: "text",
   // },
   // {
   //   shortName: "Reversed Date",
   //   label: "Enter a date, and we'll reverse it to create part of your password.",
   //   algo: "getReversedDate",
-  //   type: "text", // This option requires a text input for the date
+  //   type: "text",
   // },
   // {
   //   shortName: "Street Names",
   //   label: "Enter three street names you've passed by recently, and we'll use the first letters of each to create part of your password.",
   //   algo: "getStreetNames",
-  //   type: "text", // This option requires a text input for the street names
+  //   type: "text", 
   // },
   {
     shortName: "Random Number",
     label: "Generate a random number",
     algo: "generateRandomNumbers",
-    type: "range", // Adding range type for number length selection
-  },
-  {
-    shortName: "Random Letter",
-    label: "Generate a random letter",
-    algo: "generateRandomLetters",
-    type: "range", // Adding range type for letter length selection
+    type: "number",
   },
   {
     shortName: "Special Character",
     label: "Generate a special character or enter your own.",
     algo: "generateRandomSpecialCharacters",
-    type: "text", // Allow text input for special characters
+    type: "text", 
   },
 ];
